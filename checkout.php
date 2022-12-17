760af4007a766b064ca670329bd6fb7efb08fd27
qlifford <otienocee@gmail.com>
2022-12-17 18:01:25 +0300

    oder

--- checkout.php
+++ checkout.php
@@ -22,6 +22,8 @@ include('authenticate.php');
   <div class="py-5">
     <div class="container">            
   <div class="card-body shadow">
+    <form action="functions/placeorder.php" method="post">
+
         <div class="row">
         </div>
         <div class="row">
@@ -32,28 +34,29 @@ include('authenticate.php');
           <div class="row">
             <div class="col-md-6 mb-3">
               <label class="fw-bold">Name</label>
-              <input type="text" name="name" placeholder="Enter your full name" class="form-control">
+              <input type="text" name="name" placeholder="Enter your full name" class="form-control" >
             </div>
             <div class="col-md-6 mb-3">
               <label class="fw-bold">Email</label>
-              <input type="email" name="email" placeholder="Enter your email address" class="form-control">
+              <input type="email" name="email" placeholder="Enter your email address" class="form-control" >
             </div>
             <div class="col-md-6 mb-3">
               <label class="fw-bold">Phone</label>
-              <input type="text" name="phone" placeholder="Enter your phone number" class="form-control">
+              <input type="text" name="phone" placeholder="Enter your phone number" class="form-control"
+              >
             </div>
             <div class="col-md-6 mb-3">
               <label class="fw-bold">Pin Code</label>
-              <input type="text" name="pincode" placeholder="Enter your full name" class="form-control">
+              <input type="text" name="pincode" placeholder="Enter your full name" class="form-control" >
             </div>
             <div class="col-md-12 mb-3">
               <label class="fw-bold">Address</label>
-              <textarea type="text" name="address" class="form-control" rows="5"></textarea>
+              <textarea type="text" name="address" class="form-control" rows="5" required></textarea>
             </div>
             </div>
           </div>
           <div class="col-md-5">     
-            <h5>Order DEtails</h5>   
+            <h5>Order Details</h5>   
             <hr>          
                            
               <?php $items = getCartItems(); 
@@ -86,10 +89,12 @@ include('authenticate.php');
                         <hr>
                         <h5>Total Price : <span class="float-end fw-bold"><?= $totalPrice ?></span></h5>
                         <div class="">
-                          <button class="btn btn-primary w-100">Cornfirm an place your order | COD</button>
+                          <input type="hidden" name="payment_mode" value="COD">
+                          <button type="submit" name="placeOrderBtn" class="btn btn-primary w-100">Cornfirm an place your order | COD</button>
                         </div>
               </div>
             </div>
+            </form>
         </div>
       </div>
     </div>
---
+++ myOrders.php
@@ -0,0 +1,33 @@
+<?php
+
+include('functions/userfunctions.php'); 
+include('includes/header.php');
+include('authenticate.php');
+ 
+ ?>
+
+ <div class="py-3 bg-success">
+  <div class="container">
+    <h6 class="text-white"> 
+       <a class="text-white" href="index.php">
+        Home  
+      </a> 
+        <a class="text-white" href="myOrders.php">
+        /My Orders 
+        </a> 
+      </h6>
+    </div>
+  </div>
+
+  <div class="py-5">
+    <div class="container">           
+      <div class="">
+        <div class="row">
+              <div class="col-md-12"> 
+              </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php include('includes/footer.php'); ?>
